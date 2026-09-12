b30c68115fd96cd09b90615c32d7e836e5d6f4a2
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-09-12 09:16:51 +0200

    Adds config for downloading the GPKG from swisstopo

--- data-pipeline/config.py
+++ data-pipeline/config.py
@@ -1,6 +1,19 @@
+from datetime import date
 from pathlib import Path
 
 
 SCRIPT_DIR = Path(__file__).resolve().parent
 INPUT_PATH = Path(f"{SCRIPT_DIR}/data/source/swissBOUNDARIES3D/swissBOUNDARIES3D_1_5_LV95_LN02.gpkg")
-OUTPUT_DIR = Path(f"{SCRIPT_DIR}/data/processed")
+
+# use from https://www.swisstopo.admin.ch/en/landscape-model-swissboundaries3d
+REFERENCE_DATE = "2026-01-01"
+date.fromisoformat(REFERENCE_DATE) # validate
+
+OUTPUT_DIR = SCRIPT_DIR / "data/processed" / REFERENCE_DATE
+SWISSBOUNDARIES_RELEASE = REFERENCE_DATE[:7]
+SWISSBOUNDARIES_DOWNLOAD_URL = (
+    "https://data.geo.admin.ch/ch.swisstopo.swissboundaries3d/"
+    f"swissboundaries3d_{SWISSBOUNDARIES_RELEASE}/"
+    f"swissboundaries3d_{SWISSBOUNDARIES_RELEASE}_2056_5728.gpkg.zip"
+)
+
